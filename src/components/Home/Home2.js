715843d00fe2eaadd0eaeb998c3adeaa6694b508
salihayoubi23 <salihayoubi23@outlook.com>
2025-01-03 17:17:10 +0100

    logo

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,11 +1,9 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/avatar.svg";
+import myImg from "../../Assets/mi.png";
 import Tilt from "react-parallax-tilt";
 import {
-  AiFillGithub,
-  AiOutlineTwitter,
-  AiFillInstagram,
+  AiFillGithub
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
